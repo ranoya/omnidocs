82887bfdd03ef9f4bf2088376c0239b597ab665d
Guilherme Ranoya <ranoya@gmail.com>
2022-12-01 16:19:54 -0300

    including another plugin

---
+++ dev/bibliotecas.js
@@ -0,0 +1,60 @@
+let bibliotecas = function (par) {
+  // Change the funcion name here (imperative)
+
+  let jsonfile = `https://opensheet.elk.sh/10wpfmMWn3igQF4rJBYCo8OR90igO1tfKwcmrot0ult0/Bibliotecas`; // Change the URL here (imperative)
+
+  let namekey = `nome`; // Change the Key for the Title of the links, if needed
+  let groupkey = `linguagem`; // Change the Key for the Groups of the links, if needed
+  let linkkey = `link`; // Change the Key for the link url of the links, if needed
+
+  // Don't mess with the rest, if you don't want trouble ;-)
+
+  fetch(jsonfile)
+    .then((response) => response.json())
+    .then((jsondata) => {
+      let dados = select(jsondata, multipatterncheck_exclude, par);
+      let selectedarr = tags(dados, groupkey, ",");
+      let code = `<div class="outputgrid">`;
+      let arr = orderby(dados, selectedarr, groupkey);
+      if (arr.length > 10) {
+        for (let c = 0; c < selectedarr.length; c++) {
+          code += `<span class='categoria'><a href='javascript:addinput("${selectedarr[c]}")' class='grouplink'>${selectedarr[c]}</a></span>`;
+          for (let l = 0; l < arr.length; l++) {
+            if (arr[l]["cat"] == selectedarr[c]) {
+              if (arr[l]["Type"] == "self") {
+                code += `<a target='_self' href='javascript:be("${arr[l][linkkey]}"); toggle("poeinst");' class='linksrecursos'>${arr[l][namekey]}</a>`;
+              } else if (arr[l]["Type"] == "embed") {
+                code += `<a target='_self' href='javascript:embed("${arr[l][linkkey]}")' class='linksrecursos'>${arr[l][namekey]}</a>`;
+              } else {
+                code += `<a target='_blank' href='javascript:be("${arr[l][linkkey]}"); toggle("poeinst");' class='linksrecursos'>${arr[l][namekey]}</a>`;
+              }
+            }
+          }
+        }
+      } else {
+        let ultimoregistro = "";
+        code += `<span class='categoria'>`;
+        for (let c = 0; c < selectedarr.length; c++) {
+          code += `<a href='javascript:addinput("${selectedarr[c]}")' class='grouplink'>${selectedarr[c]}</a> • `;
+        }
+        code += `</span>`;
+        for (let l = 0; l < arr.length; l++) {
+          if (arr[l][linkkey] != ultimoregistro) {
+            if (arr[l]["Type"] == "self") {
+              code += `<a target='_self' href='javascript:be("${arr[l][linkkey]}"); toggle("poeinst");' class='linksrecursos'>${arr[l][namekey]}</a>`;
+            } else if (arr[l]["Type"] == "embed") {
+              code += `<a target='_self' href='javascript:embed("${arr[l][linkkey]}")' class='linksrecursos'>${arr[l][namekey]}</a>`;
+            } else {
+              code += `<a target='_blank' href='javascript:be("${arr[l][linkkey]}"); toggle("poeinst");' class='linksrecursos'>${arr[l][namekey]}</a>`;
+            }
+            ultimoregistro = arr[l][linkkey];
+          }
+        }
+      }
+      code += `<div>`;
+      if (arr.length == 0) {
+        code = "";
+      }
+      present(code);
+    });
+};
