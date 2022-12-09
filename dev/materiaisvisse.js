07b64d356dbb0c3960902a9ee090af45e320364e
Guilherme Ranoya <ranoya@gmail.com>
2022-12-08 22:47:13 -0300

    plugin for AND

---
+++ dev/materiaisvisse.js
@@ -0,0 +1,28 @@
+let materiais = function (par) {
+
+    let jsonfile = `https://opensheet.elk.sh/1d9eKw82lnM5GvVkBUe8wLcHSxpmDm5B6dUqWRbazOik/MATERIAIS_VISSE`;
+    
+    fetch(jsonfile).then(response => response.json()).then((jsondata) => {
+
+        if (par == "") {
+            par = "dataviz";
+        }
+
+        let arr = select(jsondata, multipatterncheck_exclude, par);
+
+        let code = `<div class="outputgrid">`;
+
+        for (let i = 0; i < arr.length; i++) {
+            code += `<a href='javascript:be(${arr[i].link}); toggle("poeinst");'>${arr[i].titulomaterial}</a>`;
+        }
+
+            code += `<div>`;
+
+            if (arr.length == 0) {
+                code = "";
+            }
+
+            present(code);
+        });
+    
+}
