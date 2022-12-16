5e48a8e582d610e284d4d51bb539c80f49353ae3
Guilherme Ranoya <ranoya@gmail.com>
2022-12-16 10:46:03 -0300

    including courses plugins for web and dgen

---
+++ dev/cursosweb.js
@@ -0,0 +1,28 @@
+let cursos = function (par) {
+  // Change the funcion name here (imperative)
+
+    let jsonfile = `https://opensheet.elk.sh/1oYqRAaiwTiYMIy832-4Knu_9MXQWiu6Nl2jmQY0_B10/Simplificado`; // Change the URL here (imperative)
+    
+    let linkkey = 'link';
+    let namekey = 'descricao';
+
+  fetch(jsonfile)
+    .then((response) => response.json())
+    .then((jsondata) => {
+      let dados = select(jsondata, multipatterncheck_exclude, "CURSOS E TUTORIAIS_LIST WEBDESIGN_DISCP " + par);
+
+      let code = `<div class="outputgrid"><span class='categoria noline'>Tutoriais e cursos online</span><span class='categoria'>&nbsp;</span>`;
+      
+      let arr = jsondata;
+            
+          for (let l = 0; l < arr.length; l++) {
+                code += `<a target='_blank' href='javascript:be("${arr[l][linkkey]}"); toggle("poeinst");' class='linksrecursos'>${arr[l][namekey]}</a>`;
+              }
+ 
+      code += `<div>`;
+      if (arr.length == 0) {
+        code = "";
+      }
+      present(code);
+    });
+};
