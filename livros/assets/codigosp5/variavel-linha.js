a09eaf5dba0f6959c771ca96838f16d7ada25387
Guilherme Ranoya <ranoya@gmail.com>
2024-05-25 17:02:58 -0300

    primeiro exemplo de p5

---
+++ livros/assets/codigosp5/variavel-linha.js
@@ -0,0 +1,21 @@
+function setup() {
+  // cria o contexto gráfico
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  // variáveis
+  var x1 = 10;
+  var y1 = 10;
+  var x2 = 100;
+  var y2 = 50;
+  var cor = "#2e8ec4";
+
+  // desenha linha
+  stroke(cor);
+  line(x1, y1, x2, y2);
+
+  // finaliza
+  noLoop();
+}
