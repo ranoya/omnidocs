8dae17e9bda96ca9e8725d1114b74e2c6d506b07
Guilherme Ranoya <ranoya@gmail.com>
2024-05-25 19:03:25 -0300

    variaveis globais para simplificar

--- livros/assets/codigosp5/variavel-linha.js
+++ livros/assets/codigosp5/variavel-linha.js
@@ -1,3 +1,10 @@
+// variáveis
+var x1 = 10;
+var y1 = 10;
+var x2 = 100;
+var y2 = 50;
+var cor = "#2e8ec4";
+
 function setup() {
   // cria o contexto gráfico
   createCanvas(windowWidth, windowHeight);
@@ -5,13 +12,6 @@ function setup() {
 }
 
 function draw() {
-  // variáveis
-  var x1 = 10;
-  var y1 = 10;
-  var x2 = 100;
-  var y2 = 50;
-  var cor = "#2e8ec4";
-
   // desenha linha
   stroke(cor);
   line(x1, y1, x2, y2);
