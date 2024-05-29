91ab49d0b08a0df93064d42ec3bc8e30173f7211
Guilherme Ranoya <ranoya@gmail.com>
2024-05-29 17:52:18 -0300

    exemplo de variável global

---
+++ livros/assets/codigojs/escopo2.js
@@ -0,0 +1,15 @@
+let A = "Estou no escopo geral";
+
+function teste1() {
+  var B = "Estou no escopo de teste1";
+  console.log(A); // funcionará
+  console.log(B); // funcionará
+}
+
+function teste2() {
+  console.log(A); // funcionará
+  console.log(B); // funcionará
+}
+
+teste1(); // executa teste1
+teste2(); // executa teste2
