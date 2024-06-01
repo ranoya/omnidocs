00eaf2f39f649dd8b0be0b8f945c1a3f628f3786
Guilherme Ranoya <ranoya@gmail.com>
2024-06-01 10:34:57 -0300

    stringtemplate.js

---
+++ livros/assets/codigojs/stringtemplate.js
@@ -0,0 +1,13 @@
+let valor = 3;
+let stringTemplate = `
+
+
+    Esta é uma String Template.
+    Ela permite que linhas sejam puladas.
+    Ela é uma das \${valor} formas de se
+    declarar uma String.
+
+    
+`;
+
+console.log(stringTemplate);
