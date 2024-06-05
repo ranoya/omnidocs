cca1fea6ccd653d6d3492e202fb37a5686edcb29
Guilherme Ranoya <ranoya@gmail.com>
2024-06-05 09:52:11 -0300

    mais condicional

---
+++ livros/assets/codigojs/condicional2.js
@@ -0,0 +1,13 @@
+let interruptor = true; // interruptor ligado
+
+if (interruptor == true) {
+  // desliga caso esteja ligado
+  interruptor = false;
+}
+
+if (interruptor == true) {
+  // liga caso esteja desligado
+  interruptor = true;
+}
+
+console.log(interruptor);
