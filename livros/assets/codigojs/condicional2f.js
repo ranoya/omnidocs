040dae2b265c904767a9ad23b342fed1e1d2da11
Guilherme Ranoya <ranoya@gmail.com>
2024-06-05 09:59:25 -0300

    código enganador

---
+++ livros/assets/codigojs/condicional2f.js
@@ -0,0 +1,13 @@
+let interruptor = false; // interruptor ligado
+
+if (interruptor == true) {
+  // desliga caso esteja ligado
+  interruptor = false;
+}
+
+if (interruptor == false) {
+  // liga caso esteja desligado
+  interruptor = true;
+}
+
+console.log(interruptor);
