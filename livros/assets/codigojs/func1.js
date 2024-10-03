042e52408ba4328eb9277539aec0457e73bfb045
Guilherme Ranoya <ranoya@gmail.com>
2024-10-03 20:44:04 -0300

    primeira função para o livro

---
+++ livros/assets/codigojs/func1.js
@@ -0,0 +1,12 @@
+let Hello = function () {
+  let objetoDate = new Date();
+  let hora = objetoDate.getHours();
+
+  if (hora >= 18) {
+    console.log("Boa Noite");
+  } else {
+    console.log("Bom dia!");
+  }
+};
+
+Hello();
