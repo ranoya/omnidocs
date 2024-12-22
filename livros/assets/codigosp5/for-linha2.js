90d29cca5fd4cfedc8838d2eee1de44c1ec00b22
Guilherme Ranoya <ranoya@gmail.com>
2024-12-21 22:45:29 -0300

    mais um exemplo

---
+++ livros/assets/codigosp5/for-linha2.js
@@ -0,0 +1,23 @@
+// variáveis
+let cor = "#2e8ec4";
+
+function setup() {
+  // cria o contexto gráfico
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  showlinegrid();
+}
+
+function draw() {
+  // apaga a tela
+  background(255);
+  showlinegrid();
+
+  // aplica a cor para a linha
+  stroke(cor);
+
+  // loop que gera o desenho
+  for (let x = 0; x < 100; x = x + 5) {
+    line(x + mouseX, 0, 0, x + mouseY);
+  }
+}
