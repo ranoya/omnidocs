fde61e6cbacd37e378650c5f0ada698aa0336915
Guilherme Ranoya <ranoya@gmail.com>
2025-01-05 21:50:41 -0300

    terceiro exemplo

--- livros/assets/codigosp5/gira3.js
+++ livros/assets/codigosp5/gira3.js
@@ -7,9 +7,11 @@ let bola = {
   py: 0,
   angulo: 0,
   raio: 50,
+  cor: "#000000",
   gira: function (mX, mY) {
     this.px = Math.cos(this.angulo) * this.raio;
     this.py = Math.sin(this.angulo) * this.raio;
+    fill(this.cor);
     ellipse(this.px + mX, this.py + mY, 20, 20);
     this.angulo = this.angulo + 0.01;
   },
@@ -21,17 +23,19 @@ let bolaC = { ...bola };
 
 bolaA.angulo = 2;
 bolaA.raio = 50;
+bolaA.cor = "#FF0000";
 
 bolaB.angulo = 4;
 bolaB.raio = 50;
+bolaB.cor = "#00FF00";
 
 bolaC.angulo = 6;
 bolaC.raio = 50;
+bolaC.cor = "#0000FF";
 
 function draw() {
   background(255);
   showlinegrid();
-  fill(0);
   bolaA.gira(mouseX, mouseY);
   bolaB.gira(mouseX, mouseY);
   bolaC.gira(mouseX, mouseY);
