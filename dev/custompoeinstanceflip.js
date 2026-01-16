2deb33110979287c06c0ea8003b9e9ab427ad4b5
Guilherme Ranoya <ranoya@gmail.com>
2026-01-16 12:41:07 -0300

    ajustes

--- dev/custompoeinstanceflip.js
+++ dev/custompoeinstanceflip.js
@@ -141,6 +141,7 @@ let createpoe = function (json, css, plugins) {
     <style>
       #poeinst {
         perspective: 600px;
+        box-shadow: 3px 0px 10px 0 var(--color-shadow) !important;
         
       }
 
